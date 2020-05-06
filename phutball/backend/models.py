3bb2f86726d74e91c3ecaf2ef8df1b17c0a8103c
Ravi Charan <ravimcharan@gmail.com>
2020-05-05 22:52:23 -0400

    set up tests for the database model and debug the model

--- phutball/backend/models.py
+++ phutball/backend/models.py
@@ -2,7 +2,8 @@
 
 from django.db import models
 from django.db.models.functions import RowNumber
-from django.db.models import F, Max, Window
+from django.db.models import F, Max, Window, Manager, Model
+from django.db.models import OuterRef, Subquery
 from .game_logic.config     import board_rows, board_cols
 from .game_logic.game_state import initial_state, GameState
 
@@ -12,8 +13,33 @@ id_len = 6
  # Longest possible jump string, approximately (comfortable upper bound)
 max_move_str_length = (board_rows * board_cols) * 2
 
-class GameManager(models.Manager):
+class Game(Model):
+  game_id       = models.CharField(max_length = id_len, unique = True, primary_key = True)	
+  date_created  = models.DateTimeField(auto_now_add=True)
+  player_0_name = models.CharField(max_length = 30, default = 'Player 1')
+  player_1_name = models.CharField(max_length = 30, default = 'Player 2')
+  ai_player     = models.BooleanField(default = False)
+  ai_player_num = models.BooleanField(default = False)
 
+  def get_history(self):
+    # Get the most recent move at each move_number
+    history = (self.move_set
+               .order_by('move_num', '-date_created')
+               .distinct('move_num')
+               .values('pk')
+    )
+
+    latest_move_per_num  = self.move_set.filter(pk__in = history)
+    latest_move_num      = latest_move_per_num.latest('date_created').move_num
+
+    history = (latest_move_per_num
+               .filter(move_num__lte = latest_move_num)
+               .order_by('move_num')
+    )
+
+    return history
+
+  @classmethod
   def new_game(self,
                ai_player = False,
                player_0_name = 'Player 1',
@@ -21,18 +47,17 @@ class GameManager(models.Manager):
                ai_player_num = False):
 
     game_id = self.new_id()
-    game    = self.create(game_id       = game_id,
-                          player_0_name = player_0_name,
-                          player_1_name = player_1_name,
-                          ai_player     = ai_player,
-                          ai_player_num = ai_player_num)
-    game.save()
+    game    = self.objects.create(game_id       = game_id,
+                                  player_0_name = player_0_name,
+                                  player_1_name = player_1_name,
+                                  ai_player     = ai_player,
+                                  ai_player_num = ai_player_num)
 
-    initial_state = Game(game_id = game_id)
-    initial_state.save()
+    initial_state = Move.objects.create(game_id = game)
 
     return game
 
+  @classmethod
   def new_id(self, recursion = 0):
     if recursion > 10:
       raise RuntimeError('Unable to generate a new game ID at random')
@@ -44,45 +69,10 @@ class GameManager(models.Manager):
       return to_try
 
 
-
-
-class Game(models.Model):
-  game_id       = models.CharField(max_length = id_len, unique = True, primary_key = True)	
-  date_created  = models.DateTimeField(auto_now_add=True)
-  player_0_name = models.CharField(max_length = 30, default = 'Player 1')
-  player_1_name = models.CharField(max_length = 30, default = 'Player 2')
-  ai_player     = models.BooleanField(default = False)
-  ai_player_num = models.BooleanField(default = False)
-
-  objects       = GameManager()
-
-  def get_history(self):
-    # Get the most recent move at each move_number
-    history = (Board.objects
-               .filter(game_id == self.game_id)
-               .annotate(
-                  move_recency = Window(
-                    expression  = RowNumber(),
-                    partitionBy = F('move_num'),
-                    orderBy     = F('date_created').desc()
-                ))
-               .filter(move_recency == 1)
-               .orderBy(date_created.asc())
-    )
-
-    latest_move_num = history.last().move_num
-
-    history = (history
-               .filter(move_num__lte = latest_move_num)
-               .orderBy(move_num.asc())
-    )
-
-    return history
-
   def __repr__(self):
     return f'{self.game_id}'
 
-class Board(models.Model):
+class Move(Model):
   date_created    = models.DateTimeField(auto_now_add=True)
   board_state     = models.CharField(max_length = board_rows * board_cols, default = initial_state)
   move_num        = models.PositiveSmallIntegerField(default = 0)
@@ -91,5 +81,7 @@ class Board(models.Model):
   ball_loc_letter_index = models.SmallIntegerField(default = 7)
   ball_loc_number_index = models.SmallIntegerField(default = 9)
 
+  def __repr__(self):
+    return f'{self.game_id.game_id} - Move {self.move_num} : {self.move_str}'
 
 
--- phutball/backend/tests.py
+++ phutball/backend/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
 
 # Create your tests here.
+from .models import Game, Move
+
+class DataModelTests(TestCase):
+	# def setUp(self):
+		# game    = Game.new_game()
+		# history = game.get_history()
+
+	def test_create_new_game_and_check_history(self):
+		game    = Game.new_game()
+		history = game.get_history()
+
+		self.assertIs(len(history), 1)
+
+	def test_branching_move_history(self):
+		'''Test resolution of two branches of a game'''
+		game    = Game.new_game()
+
+		# First Branch
+		game.move_set.create(move_num = 1, move_str = '1:1')	
+		game.move_set.create(move_num = 2, move_str = "1:2")	
+		game.move_set.create(move_num = 3, move_str = "1:3")	
+		game.move_set.create(move_num = 4, move_str = "1:4")	
+
+		# Second Branch starts at move 2
+		game.move_set.create(move_num = 2, move_str = "2:2")
+		game.move_set.create(move_num = 3, move_str = "2:3")
+
+		# Game should only return the latest updated branch
+		resolved_moves = [move.move_str for move in game.get_history()]
+		self.assertEqual(resolved_moves, ['Reset', '1:1', '2:2', '2:3'])
+
+
+
