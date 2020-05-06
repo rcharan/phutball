bd4c0e7bfbccc8bf87d359f05384fa7d91d0c674
Ravi Charan <ravimcharan@gmail.com>
2020-05-05 22:52:37 -0400

    use Postgres

--- phutball/phutball/settings.py
+++ phutball/phutball/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'phutball.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'phutball',
+        'USER': 'rcharan',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
