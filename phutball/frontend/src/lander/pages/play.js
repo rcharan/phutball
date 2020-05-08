867c6625cd3d7c3dffbc8ab11d3f2596f556c324
Ravi Charan <ravimcharan@gmail.com>
2020-05-08 01:05:58 -0400

    add content and style to the play page

--- phutball/frontend/src/lander/pages/play.js
+++ phutball/frontend/src/lander/pages/play.js
@@ -4,7 +4,49 @@ import API from '../../api'
 export default class Play extends React.Component {
   render() {
     return (
-      <GameCreator/>
+      <div className="content">
+        <div id="sub" className="subtitle" key="a">
+          Version 0.1 (May 8, 2020)
+        </div>
+        <div id="content" className="text" key="b">
+          <p id="1">
+          philosophers.football currently only supports playing against another
+          real-life person on the same computer. Workarounds are available however:</p>
+          <ul>
+            <li id="3">
+          Future versions will include bots you can play against!
+            </li>
+          <li id="2"> If you want to play against another real-life person who will use a
+          different computer, you can play pseudo-asynchronously by
+          starting a game and sending them the URL to the game, which
+          contains the unique game ID.</li>
+          </ul>
+          <p id="4">
+          If playing on different computers, be warned:
+            <ul>
+              <li key="1"> There is no authentication &ndash; nothing stops a
+              third party from interfering in your game by impersonating you,
+              or stops your opponent from playing as you. However, you
+              can always and easily undo moves.
+              </li><li key="3">Note: The Game IDs are sufficiently unique
+              that an attacker is unlikely to guess yours; hence the 
+              philosophers.football equivalent of Zoom-bombing is unlikely.</li>
+              <li key="2"> You and your opponent will have to refresh the 
+              page manually to check for new moves.</li>
+            </ul>
+          </p>
+
+        </div>
+        <div id="creator" className="creator" key="c">
+          <GameCreator/>
+        </div>
+        <div id="content" className="text" key="d">
+          <p>
+          Looking to resume a previous game? All games are saved if
+          you played online! Just find a URL like philosophers.football/game/ABC123 in your browser history.
+          </p>
+        </div>
+      </div>
     );
   }
 };
@@ -34,7 +76,7 @@ class GameCreator extends React.Component {
     if (this.state.requestSent) {
       event.preventDefault()
     } else {
-      this.setState({player0Name : event.target.value})
+      this.setState({player0Name : event.target.value.toUpperCase()})
     }
   }
 
@@ -42,7 +84,7 @@ class GameCreator extends React.Component {
     if (this.state.requestSent) {
       event.preventDefault()
     } else {
-      this.setState({player1Name : event.target.value})
+      this.setState({player1Name : event.target.value.toUpperCase()})
     }
   }
 
@@ -60,22 +102,20 @@ class GameCreator extends React.Component {
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <label>
-          Player 1 (X's) Name:
-          <input type="text" id="player0"
+        <label key="1" className="input">
+          Player 1 (X's) <input type="text" id="player0"
             value={this.state.player0Name} 
             onChange={this.handlePlayer0Change}
           />
         </label>
         <br/>
-        <label>
-          Player 2 (O's) Name:
-          <input type="text" id="player1"
+        <label key="2" className="input">
+          Player 2 (O's) <input type="text" id="player1"
             value={this.state.player1Name} 
             onChange={this.handlePlayer1Change}
           />
         </label><br/>
-        <input type="submit" value={this.state.requestSent ? 'Preparing your game' : 'Get Started!'}/>
+        <input className="submit" type="submit" value={this.state.requestSent ? 'Preparing your game' : 'Get Started!'}/>
       </form>
     )
   } 
