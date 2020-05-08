0124e91f9ae38a28f8276d0dd42c727762ac2822
Ravi Charan <ravimcharan@gmail.com>
2020-05-07 21:01:45 -0400

    re-enable latek

--- phutball/frontend/src/lander/pages/moreMath.js
+++ phutball/frontend/src/lander/pages/moreMath.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-// import 'katex/dist/katex.min.css';
+import 'katex/dist/katex.min.css';
 import { InlineMath, BlockMath } from 'react-katex';
 
 export default class MoreMath extends React.Component {
