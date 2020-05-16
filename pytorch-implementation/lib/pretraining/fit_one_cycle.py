a526954a2c3725158cab798307690642864687bd
Ravi Charan <ravimcharan@gmail.com>
2020-05-16 19:03:43 -0400

    set up fit one cycle

---
+++ pytorch-implementation/lib/pretraining/fit_one_cycle.py
@@ -0,0 +1,54 @@
+from .pre_training import random_board_batch
+from ..training import ProgressBar
+import torch.nn.functional as F
+from torch.optim import SGD
+import torch
+from ..utilities import config
+
+
+def fit_one_cycle(model):
+
+  optimizer = SGD(model.parameters(), lr = 0.01)
+  device = next(model.parameters()).device
+
+  min_density = 0
+  max_density = 0.3
+
+
+  data = []
+  min_lr = 10.0 ** -5
+  max_lr = 10.0 ** +1
+  delta  = 10.0 ** -10
+
+  loops = 60
+  lr = min_lr
+
+  bar = ProgressBar()
+
+  while lr < 10.0 ** 1
+    boards, targets = random_board_batch(
+        min_density, 
+        max_density, 
+        batch_size,
+        device
+    )
+
+    lr *= delta
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+    targets.mul_(1/config.cols)
+
+    predictions = model(boards, get_all_values = True)
+    loss        = F.mse_loss(predictions, targets)
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    bar.step()
+
+    data.append([(lr, loss)])
+
+  return data
+
