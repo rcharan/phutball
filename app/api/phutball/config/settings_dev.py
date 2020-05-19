fee51c46a879ee24412a95f9dce48451be5cdca0
Ravi Charan <ravimcharan@gmail.com>
2020-05-19 17:36:45 -0400

    fix local dev settings

--- app/api/phutball/config/settings_dev.py
+++ app/api/phutball/config/settings_dev.py
@@ -4,7 +4,7 @@ SECRET_KEY = '8u!4&6@8ojhp-pfoc%o%nw6f4&=!g^+e7n=8rxc^r8-4nuxmr!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [] # Should match service name in nginx config
+ALLOWED_HOSTS = ['localhost'] # Should match service name in nginx config
 
 CORS_ORIGIN_ALLOW_ALL = False
 
