b93816a3557901a44cfb176030aac90e63a61bfa
Ravi Charan <ravimcharan@gmail.com>
2020-06-12 15:12:27 -0400

    move progress bar to utilities

--- pytorch-implementation/lib/utilities.py
+++ pytorch-implementation/lib/utilities.py
@@ -2,6 +2,7 @@ from collections import namedtuple
 from functools import reduce
 from itertools import product as cartesian_product
 from itertools import chain
+from tensorflow.keras.utils import Progbar
 
 ###############################################################################
 #
@@ -45,3 +46,51 @@ def lfilter(predicate, iterable): return list(filter(predicate, iterable))
 # "A monad is just a monoid in the category of endofunctors, what's the problem?"
 join = chain.from_iterable
 
+
+class ProgressBar:
+  '''Wrapper for tf.keras.progbar that accounts for unknown length of game
+
+  For simplicity, the interface is slightly changed and not all features are
+  supported.
+
+  Methods
+  -------
+
+  step: increment the bar by one step. Will extend the target length if
+        the bar is more than 90% of the way done.
+
+  terminate: step the bar AND cause it to end
+
+  Attributes
+  ----------
+
+  move_num: number of moves made so far
+
+  '''
+
+  def __init__(self, estimated_length, verbose = True, expandable = True):
+    self._bar     = Progbar(estimated_length)
+    self._move_num = 0
+    self.verbose = verbose
+    self.expandable = expandable
+
+  @property
+  def move_num(self):
+    return self._move_num
+
+  def step(self):
+    self._move_num += 1
+
+    curr_target = self._bar.target
+    if self._move_num >= curr_target * 0.9 and self.expandable:
+      self._bar.target += curr_target // 10 + 1
+    
+    if self.verbose:
+      self._bar.update(self._move_num)
+
+  def terminate(self):
+    self._move_num += 1
+    self._bar.target = self._move_num
+
+    if self.verbose:
+      self._bar.update(self._move_num)
