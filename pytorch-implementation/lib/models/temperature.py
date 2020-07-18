7222419afe1be68239478326e9085f562f9aa7a4
Ravi Charan <ravimcharan@gmail.com>
2020-07-18 15:04:53 -0400

    fix deprecation warning for softmax call

--- pytorch-implementation/lib/models/temperature.py
+++ pytorch-implementation/lib/models/temperature.py
@@ -3,7 +3,7 @@ import torch.nn.functional as F
 from torch.distributions.categorical import Categorical
 
 def stochastic_move(move_vals, temp):
-  probs = F.softmax(move_vals / temp)
+  probs = F.softmax(move_vals / temp, dim = -1)
   dist  = Categorical(probs = probs)
   index = dist.sample()
   return move_vals[index], index
